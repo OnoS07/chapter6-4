5af5d3c35de0625f402d3c2c17fc3aa8189bf284
大野颯一郎 <soichrooono@gmail.com>
2020-05-08 18:07:49 +0900

    [Finish]6-6

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-$(function () {
-	$(".box1").slideDown(function(){
-	$(".box1").css({
-		"background-color": "blue",
-		"height": "100px",
-		"width": "200px"
-	  }).slideUp();
+$(function(){
+	$(".box1").on("click", function(){
+		$(".box1").addClass("box1-ext");
+	});
+
+	$(".box1").mouseout(function(){
+		$(".box1").removeClass("box1-ext");
 	});
 });
